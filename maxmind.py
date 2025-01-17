db623d30263fa4cdedfba5cf348d85299059ca79
f333333b <f33bl3n3ss@gmail.com>
2025-01-17 16:48:12 +0300

    оптимизация

--- maxmind.py
+++ maxmind.py
@@ -1,7 +1,5 @@
 import os
 import re
-import sys
-import logging
 import asyncio
 import geoip2.database
 import requests
@@ -13,12 +11,11 @@ from aiogram.types import Message
 
 TOKEN = ""
 
-# Инициализация бота и диспетчера
+# инициализация бота и диспетчера
 bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 dp = Dispatcher()
 
-
-# Функция для скачивания базы данных GeoLite2, если она отсутствует
+# функция для скачивания базы данных GeoLite2, если она отсутствует
 def download_geoip_database():
     url = 'https://storage.yandexcloud.net/maxmind-geolite2-city/GeoLite2-City.mmdb'
     if not os.path.exists('GeoLite2-City.mmdb'):
@@ -27,13 +24,11 @@ def download_geoip_database():
             file.write(get_url.content)
         print("База данных GeoLite2-City.mmdb успешно загружена.")
 
-
-# Функция для обработки текста и получения IP-адресов
+# функция для обработки текста и получения IP-адресов
 def get_ip_info(ip_input: str):
     re_format = r'\d+\.\d+\.\d+'
     ip_list = re.findall(re_format, ip_input)
     results = []
-
     if ip_list:
         with geoip2.database.Reader('GeoLite2-City.mmdb') as city_file:
             for ip in ip_list:
@@ -46,44 +41,32 @@ def get_ip_info(ip_input: str):
                     results.append(f'Произошла ошибка с IP {ip}: {e}')
     else:
         results.append('В введенном тексте IP-адреса не найдены.')
-
     return "\n".join(results)
 
-
-# Хэндлер для команды /start
+# обработка команды /start
 @dp.message(CommandStart())
-async def command_start_handler(message: Message) -> None:
-    """
-    Этот хэндлер обрабатывает команду /start
-    """
-    await message.answer(f"Привет, {html.bold(message.from_user.full_name)}! Отправьте текст с IP-адресами, и я определю их местоположение.")
+async def command_start_handler(message: Message):
+    await message.answer(f"Привет, {html.bold(message.from_user.full_name)}! Отправьте текст с IP-адресами, и я определю их местоположение")
 
-
-# Хэндлер для обработки текстовых сообщений
+# обработка сообщений
 @dp.message()
 async def handle_text(message: Message) -> None:
-    """
-    Этот хэндлер обрабатывает текстовые сообщения
-    """
-    # Скачиваем базу данных GeoLite2, если она не существует
+    # скачиваем базу данных GeoLite2, если она не существует
     download_geoip_database()
-
-    # Получаем информацию по IP-адресам из текста
+    # получаем информацию по IP-адресам из текста
     result = get_ip_info(message.text)
-    print(f"Получено сообщение: {message.text}")
-
-    # Отправляем результат обратно в чат
+    # печатаем введённое сообщение в консоль (для отладки)
+    # print(f"Получено сообщение: {message.text}")
+    # отправляем результат обратно в чат
+    # print(f"Результат: {result}")
     try:
         await message.answer(result)
     except:
         await message.answer("Слишком частые запросы. Попробуйте позже.")
 
-
-async def main() -> None:
-    # Запускаем обработку сообщений
+async def main():
+    # запуск обработки сообщений
     await dp.start_polling(bot)
 
-
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
     asyncio.run(main())
