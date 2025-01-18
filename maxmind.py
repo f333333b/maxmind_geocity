84e9952377a7937049b77138420eecc77811d5aa
chous <107110141+f333333b@users.noreply.github.com>
2025-01-18 18:46:34 +0300

    Update maxmind.py
    edited logging

--- maxmind.py
+++ maxmind.py
@@ -15,7 +15,6 @@ TOKEN = ""
 LICENSE_KEY = ''
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
-script_path = r'https://raw.githubusercontent.com/B1gG/MaxmindDownloader/refs/heads/main/maxminddownloader.sh'
 database_filename = 'GeoLite2-City.mmdb'
 url = f'https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-City&license_key=${LICENSE_KEY}&suffix=tar.gz'
 re_format = r'\d+\.\d+\.\d+'
@@ -35,35 +34,39 @@ async def download_database():
 async def is_update_needed():
     if not os.path.exists(database_filename):
         await download_database()
-    with open(database_filename, 'r') as data_file:
+    with open(database_filename, 'rb'):
         db_creation_date = datetime.fromtimestamp(os.stat(database_filename).st_ctime)
         if datetime.now() - db_creation_date > timedelta(weeks=1):
             await download_database()
 
 # функция для обработки текста и получения IP-адресов
 async def get_ip_info(ip_input: str):
-    ip_list = list(map(lambda x: x + '.0', re.findall(re_format, ip_input)))
+    ip_list_input = re.findall(re_format, ip_input)
+    ip_list = list(map(lambda x: x + '.0', ip_list_input))
     results = []
     if ip_list:
         with geoip2.database.Reader(database_filename) as city_file:
-            for ip in ip_list:
+            for i in range(len(ip_list)):
+                # сохранение IP-адреса в формате, введенном пользователем
+                ip = ip_list_input[i]
                 try:
                     response = city_file.city(ip)
-                    country = response.country.iso_code
+                    country_id = response.country.iso_code
+                    country = response.country.names.get('ru', '')
                     city = response.city.names.get('ru', '')
-                    flag = countryflag.getflag([country])
-                    results.append(f"{flag} {country} {city} <code>{ip}</code>")
+                    flag = countryflag.getflag([country_id])
+                    results.append(f"{flag} {country_id} ({country}) {city} <code>{ip}</code>")
                 except (geoip2.errors.AddressNotFoundError, ValueError) as e:
                     logging.error(f"Ошибка при обработке IP {ip}: {e}")
                     results.append(f"Ошибка при обработке IP {ip}: {e}")
     else:
-        results.append('В введенном тексте IP-адреса не найдены.')
+        results.append('В введенном тексте IP-адреса не найдены. Попробуйте ещё раз.')
     return "\n".join(results)
 
 # обработка команды /start
 @dp.message(CommandStart())
 async def command_start_handler(message: Message):
-    await message.answer(f"Привет! Отправь текст с IP-адресами, и я определю их местоположение")
+    await message.answer(f"Здравствуйте! Введите текст с IP-адресами, и я определю их местоположение (не более 100 IP-адресов за один запрос)")
 
 # обработка сообщений
 @dp.message()
@@ -74,19 +77,20 @@ async def handle_text(message: Message):
     result = await get_ip_info(message.text)
     # логирование
     user_logger = setup_user_logger(message.from_user.id)
-    print(user_logger)
     user_logger.info(f"Пользователь:{message.from_user.id}\nЗапрос:\n{message.text}\nОтвет:\n{result}")
     # отправляем результат обратно в чат
     try:
         await message.answer(result, parse_mode="HTML")
     except Exception as e:
-        await message.answer(f"При выполнении программы возникла ошибка")
+        await message.answer(f"При выполнении программы возникла ошибка. Попробуйте ещё раз.")
         logging.error(f'Ошибка {e}')
 
 def setup_user_logger(user_id):
     log_filename = f"logs/{user_id}.log"
     os.makedirs(os.path.dirname(log_filename), exist_ok=True)
     logger = logging.getLogger(str(user_id))
+    # отключение вывода логирования в консоль
+    logger.propagate = False
     handler = logging.FileHandler(log_filename, encoding='utf-8')
     formatter = logging.Formatter('%(asctime)s - %(message)s\n', datefmt='%Y-%m-%d %H:%M:%S')
     handler.setFormatter(formatter)
