e5c708f355fa1afa070134b6e414d8b942968622
f333333b <f33bl3n3ss@gmail.com>
2025-02-05 00:40:04 +0300

    updated README.md, removed f-string usage in logging

--- handlers.py
+++ handlers.py
@@ -7,6 +7,22 @@ from main_func import process_check, process_target_output, filter_ips_input, fi
 from config import user_states, user_data
 from commands import commands
 from messages import msg
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.filters import StateFilter
+
+# класс для состояния пользователя
+class UserState(StatesGroup):
+    START = State()
+    HELP = State()
+    AWAITING_BASIC_CHECK = State()
+    AWAITING_TARGET_CHECK = State()
+    AWAITING_FILTER_LIST_FIRST = State()
+    AWAITING_FILTER_LIST_SECOND = State()
+    AWAITING_FILTER_OCTET_FIRST = State()
+    AWAITING_FILTER_OCTET_SECOND = State()
+    AWAITING_DATABASE_UPDATE = State()
+    COPY_IPS = State()
 
 router = Router()
 
@@ -16,62 +32,83 @@ router = Router()
 async def handle_unsupported_content(message: Message):
     return await message.answer(msg['invalid_input'])
 
-# обработка команды /start
-@router.message(CommandStart())
+@router.message(UserState.START)
 @log_interaction
 async def command_start_handler(message: Message):
+    """Обработка команды /start"""
     return await message.answer('Здравствуйте! ' + msg['start'], reply_markup=keyboard_main)
-    user_state[user_id] = 'start'
 
-# обработка команды /help
-@router.message(Command("help"))
+@router.message(UserState.HELP)
 @log_interaction
 async def command_help_handler(message: Message):
+    """Обработка команды /help"""
     return await message.answer(msg['help'], parse_mode='HTML')
 
-# сценарий № 1: определение геолокации IP-адресов
-@router.message(Command("check"))
+@router.message(UserState.AWAITING_BASIC_CHECK)
 @log_interaction
 async def command_check_handler(message: Message):
-    user_id = message.from_user.id
-    user_states[user_id] = 'awaiting_basic_check'
+    """Определение геолокации IP-адресов"""
     return await message.answer(msg["enter_check_ips"])
 
-# сценарий № 2: определение геолокации IP-адресов с фильтрацией по стране
-@router.message(Command("target"))
+@router.message(UserState.AWAITING_TARGET_CHECK)
 @log_interaction
 async def command_target_handler(message: Message):
-    user_id = message.from_user.id
-    user_states[user_id] = 'awaiting_target_check'
+    """Определение геолокации IP-адресов с фильтрацией по стране"""
     return await message.answer(msg['enter_target_ips'])
 
-# сценарий № 3: фильтрация списков IP-адресов (основной список)
-@router.message(Command("filter"))
-@log_interaction
-async def command_filter_handler(message: Message):
-    user_id = message.from_user.id
-    user_states[user_id] = 'awaiting_filter_first_input'
-    return await message.answer(msg['filter_ips_first'])
-
-# сценарий № 4: фильтрация по октету (ввод октета)
-@router.message(Command("filter_octet"))
-@log_interaction
-async def command_filter_octet_handler(message: Message):
-    user_id = message.from_user.id
-    user_states[user_id] = 'awaiting_filter_octet_list'
-    return await message.answer(msg['filter_ips_first'])
-
-# обработка callback-запросов от Inline-кнопок
-@router.callback_query()
+@router.callback_query(StateFilter(UserState.COPY_IPS))
 @log_interaction
 async def handle_callback(query: CallbackQuery):
-    user_id = query.from_user.id
-    if query.data == 'copy_ips':
-        result_copy = user_data.get(user_id, [])
-        formatted_ips, error_message = await process_target_output(result_copy)
-        user_states[user_id] = 'awaiting_target_check'
-        text = error_message or formatted_ips
-        return await query.message.answer(text=text, parse_mode="HTML")
+    """Обработка нажатия кнопки 'Вывести строки с IP-адресами указанной страны'"""
+    #user_id = query.from_user.id
+    #if query.data == 'copy_ips':
+    #    result_copy = user_data.get(user_id, [])
+    #    formatted_ips, error_message = await process_target_output(result_copy)
+    #    user_states[user_id] = 'awaiting_target_check'
+    #    text = error_message or formatted_ips
+    #    return await query.message.answer(text=text, parse_mode="HTML")
+
+@router.message(UserState.AWAITING_FILTER_LIST_FIRST)
+@log_interaction
+async def command_filter_handler(message: Message):
+    """Обработка команды /filter - фильтрация списков IP-адресов (основной список)"""
+    return await message.answer(msg['filter_ips_first'])
+
+@router.message(UserState.AWAITING_FILTER_LIST_SECOND)
+@log_interaction
+async def command_filter_octet_handler(message: Message):
+    """Обработка ввода второго списка при фильтрации списков IP-адресов"""
+    return await message.answer(msg['filter_ips_second'])
+
+@router.message(UserState.AWAITING_FILTER_OCTET_FIRST)
+@log_interaction
+async def command_filter_octet_handler(message: Message):
+    """Обработка команды /filter_octet - фильтрация списков IP-адресов по октету (ввод списка)"""
+    return await message.answer(text=await filter_ips_input(message.text, list_flag=False), parse_mode="HTML")
+
+@router.message(UserState.AWAITING_FILTER_OCTET_SECOND)
+@log_interaction
+async def command_filter_octet_handler(message: Message):
+    """Обработка ввода октета  при фильтрации списков IP-адресов по октету"""
+    return await message.answer(text=await filter_by_octet(message.text), parse_mode="HTML")
+
+@router.message(UserState.AWAITING_DATABASE_UPDATE)
+@log_interaction
+async def command_filter_octet_handler(message: Message):
+    """Обновление базы данных"""
+    return await message.answer(msg['db_updating'])
+
+
+#новые хендлеры
+@router.message(UserState.AWAITING_TARGET_CHECK)
+@log_interaction
+async def handle_target_check(message: Message, state: FSMContext):
+    """Определение геолокации IP-адресов с фильтрацией по стране"""
+    user_id = message.from_user.id
+    await process_check('awaiting_target_check', message, user_id, target_flag=True)
+
+
+
 
 # обработка сообщений
 @router.message()
@@ -89,40 +126,7 @@ async def handle_text(message: Message):
     if message.text.startswith('/') and not any(message.text == command.command for command in commands):
         return await message.answer(msg['invalid_command'])
 
-    # стандартное состояние ('start')
-    if user_state == 'start':
-        return await message.answer(msg['start'], reply_markup=keyboard_main)
 
-    # сценарий № 1: определение геолокации IP-адресов с фильтрацией по стране
-    elif user_state == 'awaiting_target_check':
-        await process_check('awaiting_target_check', message, user_id, target_flag=True)
-
-    # сценарий # 2: определение геолокации IP-адресов
-    elif user_state == 'awaiting_basic_check':
-        await process_check('awaiting_basic_check', message, user_id, target_flag=False)
-
-    # сценарий № 3: фильтрация списков IP-адресов по списку (ввод первого списка)
-    elif user_state == 'awaiting_filter_first_input':
-        return await message.answer(text=await filter_ips_input(message.text, user_id, list_flag=True), parse_mode="HTML")
-
-    # сценарий № 4: фильтрация списков IP-адресов по списку (ввод второго списка)
-    elif user_state == 'awaiting_filter_second_input':
-        return await message.answer(text=await filter_ips_list(message.text, user_id), parse_mode="HTML")
-
-    # сценарий № 5: фильтрация списка IP-адресов по первому октету (ввод списка)
-    elif user_state == 'awaiting_filter_octet_list':
-        return await message.answer(text=await filter_ips_input(message.text, user_id, list_flag=False), parse_mode="HTML")
-
-    # сценарий № 6: фильтрация списка IP-адресов по первому октету (ввод октета)
-    elif user_state == 'awaiting_filter_by_octet':
-        return await message.answer(text=await filter_by_octet(message.text, user_id), parse_mode="HTML")
-
-    # сценарий № 7: обновление базы данных в процессе
-    elif user_state == 'awaiting_database_update':
-        return await message.answer(msg['db_updating'])
-
-    else:
-        return await message.answer(msg['start'], parse_mode="HTML")
 
 def register_handlers(dp):
     dp.include_router(router)
